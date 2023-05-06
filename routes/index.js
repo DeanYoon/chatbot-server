3fa990dedc16e945f4c5fdd92946f83f90977b5d
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-05-06 20:07:38 +0900

    deployed url test

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 router.use(
   cors({
     origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
-    methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS", "HEAD", "PATCH"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
     credentials: true,
   })
@@ -13,6 +13,10 @@ router.use(
 
 router.get("/", function (req, res, next) {
   res.send({ message: "Welcome to the API server" });
+});
+
+router.get("/test", function (req, res, next) {
+  res.send({ message: "Welcome to the deployed API server" });
 });
 
 module.exports = router;
